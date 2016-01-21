dc1b7ff467995e5867c64bd2b53361c2ecf3cfec
Ankit Gupta <gupta.ankit2791@gmail.com>
2016-01-22 03:08:42 +0530

    Update urls.py

--- minbook/urls.py
+++ minbook/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^bookmarks/', include('bookmarks.urls')),
+    url(r'^$', include('bookmarks.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
