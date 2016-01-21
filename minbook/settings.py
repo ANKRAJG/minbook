69b8eb05d204bd93cc2731f4d87fc0cb58b9d2da
Ankit Gupta <gupta.ankit2791@gmail.com>
2016-01-21 20:40:28 +0530

    Update settings.py

--- minbook/settings.py
+++ minbook/settings.py
@@ -99,7 +99,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/static'),
+)
+
+
+
+
+
+
+
